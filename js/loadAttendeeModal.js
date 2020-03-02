e2cf8ed593239fb9816af65ca4b7a3915b4425b5
Ari <Ari>
2020-03-02 14:12:53 -0500

    Added relative path to view attendees

--- js/loadAttendeeModal.js
+++ js/loadAttendeeModal.js
@@ -1,7 +1,14 @@
 function loadAttendeeModal(event, type) {
-	let url = window.location.protocol + '//' + window.location.host + '/server-dev/projects/Project%201/event-management-system/templates/view_attendees.php?event=' 
-	+ event + '&type=' + type;
+	let parts = window.location.pathname.split('/');
+	let relativePath = '';
 
+	for(let i = 0; i <= 4; i++) {
+		relativePath += parts[i] + '/';
+	}
+
+	let url = window.location.protocol + '//' + window.location.host + relativePath + 'templates/view_attendees.php?event=' 
+	+ event + '&type=' + type;
+	
 	let modal = $('#attendees');
 	modal.find('.modal-content').load(url);
 	modal.modal('show');
