9b67565d91fdf44ca94ca1d135e8e8288da61767
Ari <Ari>
2020-03-02 14:30:53 -0500

    Small UI changes to manager event table

--- components/manager/manage.php
+++ components/manager/manage.php
@@ -27,99 +27,115 @@
 	<?php 
 		getMessage();
 	?>
-
-	<table class="table">
-	  <thead class="thead-dark">
-	    <tr>
-	      <th scope="col"></th>
-	      <th scope="col">Name</th>
-	      <th scope="col">Start date</th>
-	      <th scope="col">End date</th>
-	      <th scope="col">Number of participants</th>
-	      <th scope="col">Venue</th>
-	      <th scope="col"></th>
-	      <th scope="col" colspan="2"><button type="button" class="btn btn-warning" data-type="event" data-toggle="modal" data-target="#addEvent">Add an Event</button></th>
-	    </tr>
-	  </thead>
-	  <tbody>
-	  	<?php if(count($events) > 0) {
-	  		foreach($events as $event) { 
-	  			$info = new Event();
-	  			$info->setIdEvent($event->getEvent());
-	  			$info = $info->getEvent();
-	  			// var_dump($info);
-	  			$venue = new Venue();
-	  			$venue->setIdVenue($info->getVenueId());
-	  			$venueName = $venue->getVenue()->getName();
-	  			$sessions = $info->getSessions();
-	  	?>
-	  	<thead class="thead-light">
+	<div class="table-responsive">
+		<table class="table">
+		  <thead class="thead-dark">
 		    <tr>
-		      <th scope="col">
-		      	<button type="button" class="btn btn-warning icon" onclick="loadAttendeeModal(<?=$info->getIdEvent()?>,'event')" data-href="<?=HTTP_URL ?>/templates/view_attendees.php"><i class="fas fa-users fa-lg"></i></button>
-		      </th>
+		      <th scope="col"></th>
+		      <th scope="col">Name</th>
+		      <th scope="col">Start date</th>
+		      <th scope="col">End date</th>
+		      <th scope="col">Number of participants</th>
+		      <th scope="col">Venue</th>
+		      <th scope="col"></th>
+		      <th scope="col" colspan="2"><button type="button" class="btn btn-warning" data-type="event" data-toggle="modal" data-target="#addEvent">Add an Event</button></th>
+		    </tr>
+		  </thead>
+		  <tbody>
+		  	<?php if(count($events) > 0) {
+		  		foreach($events as $event) { 
+		  			$info = new Event();
+		  			$info->setIdEvent($event->getEvent());
+		  			$info = $info->getEvent();
+		  			// var_dump($info);
+		  			$venue = new Venue();
+		  			$venue->setIdVenue($info->getVenueId());
+		  			$venueName = $venue->getVenue()->getName();
+		  			$sessions = $info->getSessions();
+		  	?>
+		  	<thead class="thead-light">
+			    <tr>
+			      <th>
+			      	<button type="button" class="btn btn-warning icon" onclick="loadAttendeeModal(<?=$info->getIdEvent()?>,'event')" data-href="<?=HTTP_URL ?>/templates/view_attendees.php"><i class="fas fa-users fa-lg"></i></button>
+			      </th>
 
-		      <th><?=$info->getName() ?></th>
-		      <th scope="col"><?=$info->getDateStart() ?></th>
-		      <th scope="col"><?=$info->getDateEnd() ?></th>
-		      <th scope="col"><?=$info->getNumberallowed() ?></th>
-		      <th scope="col"><?=$venueName ?></th>
+			      <th><?=$info->getName() ?></th>
+			      <th><?=$info->getDateStart() ?></th>
+			      <th><?=$info->getDateEnd() ?></th>
+			      <th><?=$info->getNumberallowed() ?></th>
+			      <th><?=$venueName ?></th>
 
-     		  <th scope="col">
-		      	<button type="button" class="btn btn-warning icon" data-toggle="modal" data-target="#addSession" data-event="<?=$info->getIdEvent() ?>" data-max="<?=$info->getNumberallowed() ?>" data-name="<?=$info->getName() ?>"><i class="fas fa-plus-square fa-lg"></i></button>
-		      </th>
+	     		  <th>
+			      	<button type="button" class="btn btn-warning icon" data-toggle="modal" data-target="#addSession" data-event="<?=$info->getIdEvent() ?>" data-max="<?=$info->getNumberallowed() ?>" data-name="<?=$info->getName() ?>">
+			      		<i class="fas fa-plus-square fa-lg"></i>
+			      	</button>
+			      </th>
 
-		      <th scope="col">
-		      	<button type="button" class="btn btn-warning icon" data-toggle="modal" data-type="event" data-target="#editEvent" data-details="<?=$info->toArray() ?>"><i class="fas fa-edit fa-lg"></i></button>
-		      </th>		 
+			      <th>
+			      	<button type="button" class="btn btn-warning icon" data-toggle="modal" data-type="event" data-target="#editEvent" data-details="<?=$info->toArray() ?>">
+			      		<i class="fas fa-edit fa-lg"></i>
+			      	</button>
+			      </th>		 
 
-		      <th scope="col">
-		      	<form method="post" action="delete_event.php?type=event">
-		      		<input type="hidden" name="id" value="<?=$info->getIdEvent() ?>" />
-		      		<button type="submit" class="btn btn-warning icon"><i class="fas fa-trash-alt fa-lg"></i></button>
+			      <th>
+			      	<form method="post" action="delete_event.php?type=event">
+			      		<input type="hidden" name="id" value="<?=$info->getIdEvent() ?>" />
+			      		<button type="submit" class="btn btn-warning icon">
+			      			<i class="fas fa-trash-alt fa-lg"></i>
+			      		</button>
+			      	</form>
+			      </th>
+
+			      
+			    </tr>
+			 </thead>
+			<?php 
+				if(count($sessions) > 0) {
+					foreach($sessions as $session) {
+			?>
+		    <tr>
+		      <td>
+		      	<button type="button" class="btn btn-warning icon" onclick="loadAttendeeModal(<?=$session->getIdSession()?>,'session')" data-href="<?=HTTP_URL ?>/templates/view_attendees.php">
+		      		<i class="fas fa-users fa-lg"></i>
+		      	</button>
+		      </td>
+
+		      <td><?=$session->getName() ?></td>
+		      <td><?=$session->getStartdate() ?></td>
+		      <td><?=$session->getEnddate() ?></td>
+		      <td><?=$session->getNumberallowed() ?></td>
+		      <td></td>
+		      <td></td>
+
+		      <td>
+		      	<button type="button" class="btn btn-warning icon" data-toggle="modal" data-type="session" data-target="#editEvent" data-details="<?=$session->toArray() ?>">
+		      		<i class="fas fa-edit fa-lg"></i>
+		      	</button>
+		      </td>
+
+		      <td>
+		      	<form method="post" action="delete_event.php?type=session">
+		      		<input type="hidden" name="id" value="<?=$session->getIdSession() ?>" />
+		      		<button type="submit" class="btn btn-warning icon">
+		      			<i class="fas fa-trash-alt fa-lg"></i>
+		      		</button>
 		      	</form>
-		      </th>
+		      </td>
 
 		      
 		    </tr>
-		 </thead>
-		<?php 
-			if(count($sessions) > 0) {
-				foreach($sessions as $session) {
-		?>
-	    <tr>
-	      <td>
-	      	<button type="button" class="btn btn-warning icon" onclick="loadAttendeeModal(<?=$session->getIdSession()?>,'session')" data-href="<?=HTTP_URL ?>/templates/view_attendees.php"><i class="fas fa-users fa-lg"></i></button>
-	      </td>
-
-	      <td><?=$session->getName() ?></td>
-	      <td><?=$session->getStartdate() ?></td>
-	      <td><?=$session->getEnddate() ?></td>
-	      <td><?=$session->getNumberallowed() ?></td>
-	      <td></td>
-	      <td></td>
-
-	      <td><button type="button" class="btn btn-warning icon" data-toggle="modal" data-type="session" data-target="#editEvent" data-details="<?=$session->toArray() ?>"><i class="fas fa-edit fa-lg"></i></button></td>
-	      <td>
-	      	<form method="post" action="delete_event.php?type=session">
-	      		<input type="hidden" name="id" value="<?=$session->getIdSession() ?>" />
-	      		<button type="submit" class="btn btn-warning icon"><i class="fas fa-trash-alt fa-lg"></i></button>
-	      	</form>
-	      </td>
-
-	      
-	    </tr>
-		<?php
-					} // foreach session
-				} // if count session
-			}// foreach event 
-		} // if count event
-		 else {
-		 	echo '<tr><td><h3>No events found!</h3></td></tr>';
-		 }
-		?>
-	  </tbody>
-	</table>
+			<?php
+						} // foreach session
+					} // if count session
+				}// foreach event 
+			} // if count event
+			 else {
+			 	echo '<tr><td><h3>No events found!</h3></td></tr>';
+			 }
+			?>
+		  </tbody>
+		</table>
+	</div>
 
 
 	<div class="modal fade" id="attendees" tabindex="-1" role="dialog" aria-hidden="true">
